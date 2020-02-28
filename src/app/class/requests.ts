9a2b028ade20f127cbdb719fc74d7b7ff0802cd4
unknown <gospeltruth18@gmail.com>
2020-02-28 14:09:55 +0100

    fb auth

---
+++ src/app/class/requests.ts
@@ -0,0 +1,12 @@
+export class Requests {
+constructor(
+    public username: any,
+    public email: any,
+    public password: any,
+    public name: any,
+    public address: any,
+    public number: any,
+    public date_of_birth: any,
+
+) {}
+}
